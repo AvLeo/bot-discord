6f7d5ad66f3348fdfe67fd53dd509cfcba98218f
Avleo <leito.avila.sm@gmail.com>
2024-03-05 11:46:45 -0300

    add delete channels voice in category Alumnos with node-cron

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,18 +1,39 @@
+const {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ChannelType
+} = require('discord.js')
 const { loadCommands } = require('../../Handlers/commandHandler');
-const config = require('../../config.json')
-const mongoose = require('mongoose')
+const discordID = require('../../discordID.json')
+const cron = require('node-cron')
 
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
-      console.log("El cliente ya esta listo");
-
-      // await mongoose.connect(config.mongopass,{
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology:true
-      // }).then(res => console.log("Base de datos Conectada")).catch(error => console.error(error))
 
       loadCommands(client)
+      
+      cron.schedule('* 3 * * *', () => {
+        
+        let deletedChannels = []
+        
+        const channelsInCategory = client.channels.cache.filter(channel => channel.parentId === discordID.category.alumnos)
+        
+        channelsInCategory.filter(channel  => channel.type === 2 ? deletedChannels.push(channel.id): false)
+        
+        if(deletedChannels.length > 0){
+          deletedChannels.forEach( channel => {
+            const channelActive = client.channels.cache.get(channel);
+            channelActive.delete()
+          })
+        }else{
+          console.log("No hay canales de voz activos en Alumnos")
+        }
+  
+      });
+
+      
     },
 };
